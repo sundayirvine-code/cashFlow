11a048fc3b942c38d3c71b7fa0b58ab0ce74685b
Irvine Sunday <amugumbiirvine@gmail.com>
2023-09-10 15:20:57 +0300

    Update transactions.py
    created calculate_expense_totals a function that filters expense transactions by date

--- transactions.py
+++ transactions.py
@@ -100,3 +100,67 @@ def calculate_income_totals(user_id, start_date=None, end_date=None):
         income_totals[category.name] = total_amount
 
     return income_totals
+
+def calculate_expense_totals(user_id, start_date=None, end_date=None):
+    """
+    Calculate the total expense amounts for each expense category of a user within a specified date range.
+
+    Args:
+        user_id (int): The user's ID for whom expense totals are calculated.
+        start_date (date, optional): The start date of the date range (inclusive). If not provided, it defaults to the
+            beginning of the current month.
+        end_date (date, optional): The end date of the date range (inclusive). If not provided, it defaults to the
+            current date.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary contains 'name', 'amount', and 'percentage' keys.
+
+    Example:
+        # Calculate expense totals for the current user for the current month
+        user_id = current_user.id
+        expense_totals = calculate_expense_totals(user_id)
+        print(expense_totals)
+        
+        # Calculate expense totals for the current user for a custom date range
+        start_date = date(2023, 1, 1)
+        end_date = date(2023, 1, 31)
+        expense_totals = calculate_expense_totals(user_id, start_date, end_date)
+        print(expense_totals)
+    """
+    from datetime import date
+    from models import Expense, CashOut
+    
+    # If start_date is not provided, set it to the beginning of the current month
+    if not start_date:
+        today = date.today()
+        start_date = date(today.year, today.month, 1)
+
+    # If end_date is not provided, set it to the current date
+    if not end_date:
+        end_date = date.today()
+
+    # Query all expense categories for the user
+    expense_categories = Expense.query.filter_by(user_id=user_id).all()
+
+    # Initialize a list to store expense category totals
+    expense_totals = {}
+
+    # Iterate through each expense category
+    for category in expense_categories:
+        # Query CashOut transactions for the specific expense category within the date range
+        total_amount = CashOut.query.filter(
+            CashOut.user_id == user_id,
+            CashOut.expense_id == category.id,
+            CashOut.date >= start_date,
+            CashOut.date <= end_date
+        ).with_entities(db.func.sum(CashOut.amount)).scalar()
+
+        # If there are no transactions, set total_amount to 0
+        if total_amount is None:
+            total_amount = 0
+
+        # Add the income category name and total amount to the income_totals dictionary
+        expense_totals[category.name] = total_amount
+
+    return expense_totals
+
