f813a67f576f81f99e02e9f08e5d761c65c89cf2
Irvine Sunday <amugumbiirvine@gmail.com>
2023-09-10 14:23:39 +0300

    Update models.py
    Created Income and IncomeType models

--- models.py
+++ models.py
@@ -27,3 +27,37 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"<User {self.id}: {self.first_name} {self.last_name}>"
 
+class IncomeType(db.Model):
+    """
+    Represents different types of income sources.
+
+    Attributes:
+        id (int): The unique identifier for the income type.
+        name (str): The name of the income type.
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<IncomeType {self.name}>"
+
+class Income(db.Model):
+    """
+    Represents an income category associated with a user and an income type.
+
+    Attributes:
+        id (int): The unique identifier for the income transaction.
+        name (str): The name of the income.
+        user_id (int): The foreign key referencing the associated User.
+        income_type_id (int): The foreign key referencing the associated IncomeType.
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    income_type_id = db.Column(db.Integer, db.ForeignKey('income_type.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Income {self.name}>"
+
